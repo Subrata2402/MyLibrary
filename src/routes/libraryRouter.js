20a4c5b98ffbdc227734a10c558c7a8934b7e6ce
subrata2402 <subratadas3250@gmail.com>
2023-11-14 23:32:40 +0530

    update edit and delete quesiton

--- src/routes/libraryRouter.js
+++ src/routes/libraryRouter.js
@@ -56,18 +56,44 @@ router.get("/deleteq", async (req, res) => {
 router.get("/editq", async (req, res) => {
     if (req.cookies.jwt) {
         if (!req.query._id) return res.render("library", {isAuthenticated: req.cookies.jwt});
+        const data = await q_and_t(req.query._id, req.query.topic);
+        const question = await Question.findById(req.query._id);
+        res.render("addq", {isAuthenticated: req.cookies.jwt, _id: req.query._id, title: data.title, question: question.question, answer: question.answer, topic: question.topic});
+    } else {
+        res.render("login", {isAuthenticated: req.cookies.jwt});
     }
 });
 
 router.get("/questions", async (req, res) => {
     if (req.cookies.jwt) {
         if (!req.query.topic) return res.render("library", {isAuthenticated: req.cookies.jwt});
-        const questions = await Question.find({topic: req.query.topic});
-        const _title = req.query.topic.replace("-", " ").replace("and", "&").replace("-", " ").replace("-", " ");
-        const title = _title.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
-        res.render("questions", {isAuthenticated: req.cookies.jwt, questions: questions, title: title});
+        const data = await q_and_t(req.query._id, req.query.topic);
+        res.render("questions", {isAuthenticated: req.cookies.jwt, questions: data.questions, title: data.title});
     } else {
         res.render("login", {isAuthenticated: req.cookies.jwt});
+    }
+});
+
+router.post("/editq", async (req, res) => {
+    // console.log(req.body);
+    try {
+        const _id = req.query._id;
+        const topic = req.query.topic;
+        const question = req.body.question;
+        const answer = req.body.answer;
+        const questionData = await Question.findById(_id);
+        let data = await q_and_t(_id, topic)
+        if (!questionData) return res.render("questions", {isAuthenticated: req.cookies.jwt, message: "Question not found.", messageStatus: "Error!", questions: data.questions, title: data.title});
+        if (questionData.author_id === jwt.verify(req.cookies.jwt, process.env.SECRET_KEY)._id) {
+            await Question.updateOne({_id: _id}, {$set: {question: question, answer: answer}});
+            let data = await q_and_t(_id, topic);
+            res.render("questions", {isAuthenticated: req.cookies.jwt, message: "Question updated successfully.", messageStatus: "Success!", questions: data.questions, title: data.title});
+        } else {
+            res.render("questions", {isAuthenticated: req.cookies.jwt, message: "You are not authorized to edit this question.", messageStatus: "Error!", questions: data.questions, title: data.title});
+        }
+    } catch (error) {
+        // console.log(error);
+        res.status(401).send(error);
     }
 });
 
